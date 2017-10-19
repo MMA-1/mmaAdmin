aac5b84a9c4cf9ba568ac7ad09186751088e3443
MMA-1 <mazharabbas.official@gmail.com>
2017-10-19 23:42:56 +0530

    apply template , add js and css files remove php errors.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,90 +1,54 @@
 <!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<!--<![endif]-->
+
+<!-- BEGIN HEAD -->
+<head>
+  <meta charset="UTF-8" />
     <title>@yield('title')</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  {{Html::style('assets/plugins/bootstrap/css/bootstrap.css')}}
+  {{Html::style('assets/css/main.css')}}
+  {{Html::style('assets/css/theme.css')}}
+  {{Html::style('assets/css/MoneAdmin.css')}}
+  {{Html::style('assets/plugins/Font-Awesome/css/font-awesome.css')}}
+  <!--END GLOBAL STYLES -->
 
-    <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
-   {{Html::style('css/jquery-sakura.min.css')}}
-    {{Html::style('css/bootstrap.min.css')}}
-    {{Html::style('css/styles.css')}}
+  <!-- END PAGE LEVEL  STYLES -->
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
     @yield('stylesheet')
   </head>
-  <body>
-    <!-- Default Vavbar  -->
-    <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Shayeri Urdu</a>
-    </div>
+  <body class="padTop53">
+  <div id="wrap">
+  <!-- HEADER SECTION -->
+  @include('partials._adminheader')
+  <!-- END HEADER SECTION -->
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        {{--<li class="{{Request::is('/')? "active":""}}"><a href="/">Home <span class="sr-only">(current)</span></a></li>--}}
-        {{--<li class="{{Request::is('shayari')? "active":""}}"><a href="/blog">Blog</a></li>--}}
-        {{--<li class="{{Request::is('about')? "active":""}}"><a href="/about">About</a></li>--}}
-        {{--<li class="{{Request::is('contact')? "active":""}}"><a href="/contact">Contact</a></li>--}}
-       
-      </ul>
-     @if(Auth::check())
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hello {{Auth::user()->name}} <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="{{route('posts.index')}}">List Posts</a></li>
-            <li><a href="{{route('posts.create')}}">Create Post</a></li>
-            <li><a href="{{route('categories.index')}}">Categories</a></li>
-            <li><a href="{{route('subcategories.index')}}">Sub Categories</a></li>
-            <li><a href="{{route('tags.index')}}">Tags</a></li>
-            <li><a href="{{route('gallery.create')}}">Create Photo Gallery</a></li>
-            <li><a href="{{route('gallery.index')}}">List Photo Gallery</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="{{route('logout')}}">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-       @else
-        <a href="{{route('login')}}" class="btn btn-success pull-right">Login</a></li>
-      @endif
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-<div class="container">
+  <!-- MENU SECTION -->
+  @include('partials._adminsidenav')
+  <!--END MENU SECTION -->
+<div id="content">
     @include('partials._messages')
   {{Auth::check()? "Logged In":"Logged Out"}}
    @yield('content')
 
 
    <hr>
-   <p>Copyright MMA - All right reserved.</p>
-</div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>--}}
-    {{Html::script('js/jquery.min.js')}}
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</div>
+  </div>
+  <div id="footer">
+    <p>&copy;  binarytheme &nbsp;2014 &nbsp;</p>
+  </div>
+    {{Html::script('assets/plugins/jquery-2.0.3.min.js')}}
+    {{Html::script('assets/plugins/bootstrap/js/bootstrap.min.js')}}
    @yield('scripts')
   </body>
 </html>
---
+++ resources/views/partials/_adminsidenav.blade.php
@@ -0,0 +1,39 @@
+<div id="left">
+	<div class="media user-media well-small"> <a class="user-link" href="#"> <img class="media-object img-thumbnail img-responsive user-img" alt="User Picture" src="{{asset('assets/img/mma64x48.png')}}" /> </a> <br />
+		<div class="media-body">
+			<h5 class="media-heading"> Mazhar</h5>
+			<ul class="list-unstyled user-info">
+				<li> <a class="btn btn-success btn-xs btn-circle" style="width: 10px;height: 12px;"></a> Online </li>
+			</ul>
+		</div>
+		<br />
+	</div>
+	<ul id="menu" class="collapse">
+		@if(Auth::check())
+		<li class="panel"> <a href="index.html" > <i class="icon-table"></i> Dashboard </a> </li>
+		<li class="panel active"> <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#idpost"> <i class="icon-columns"></i> Posts <span class="pull-right"> <i class="icon-angle-left"></i> </span> &nbsp; <span class="label label-success">5</span>&nbsp; </a>
+			<ul class="collapse" id="idpost">
+				<li class=""><a href="{{route('posts.create')}}"><i class="icon-angle-right"></i>Create Post</a></li>
+				<li class=""><a href="{{route('posts.index')}}"><i class="icon-angle-right"></i>List Post</a></li>
+			</ul>
+		</li>
+		<li class="panel"> <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#idcategory"> <i class="icon-sitemap"></i> Categories <span class="pull-right"> <i class="icon-angle-left"></i> </span> &nbsp; <span class="label label-success">7</span>&nbsp; </a>
+			<ul class="collapse" id="idcategory">
+				<li class=""><a href="{{route('categories.index')}}"><i class="icon-angle-right"></i>Categories</a></li>
+				<li class=""><a href="{{route('subcategories.index')}}"><i class="icon-angle-right"></i>Sub Categories</a></li>
+				<li class=""><a href="{{route('tags.index')}}"><i class="icon-angle-right"></i>Tags</a></li>
+			</ul>
+		</li>
+		<li class="panel"> <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#idgallery"> <i class="icon-film"></i> Gallery <span class="pull-right"> <i class="icon-angle-left"></i> </span> &nbsp; <span class="label label-success">5</span>&nbsp; </a>
+			<ul class="collapse" id="idgallery">
+				<li class=""><a href="{{route('gallery.create')}}"><i class="icon-angle-right"></i>Create Photo Gallery</a></li>
+				<li class=""><a href="{{route('gallery.index')}}"><i class="icon-angle-right"></i>List Photo Gallery</a></li>
+			</ul>
+		</li>
+
+			<li><a href="{{route('logout')}}">Logout</a></li>
+		@else
+			<a href="{{route('login')}}" class="btn btn-success pull-right">Login</a></li>
+		@endif
+	</ul>
+</div>
